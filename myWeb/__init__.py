95a1c0ae3081f0cc0c4edee23784e92a24acb1f4
lhs7091 <lhs7091@naver.com>
2020-04-26 14:03:21 +0900

    flask project
    Templates

--- myWeb/__init__.py
+++ myWeb/__init__.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted': 'April 20 2018'
+    },
+    {
+        'author': 'Lee Heesoo',
+        'title': 'Flask Project',
+        'content': 'First Flask',
+        'date_posted': 'April 26 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def hello():
     #return "<h1>Hello Flask</h1>"
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
     #return "<h1>About Flask</h1>"
 
 
